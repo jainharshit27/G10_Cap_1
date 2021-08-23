c4949db39676ac62e3f8f75db7192fb59f5a0445
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-08-23 10:27:26 +0530

    Update C17_2.py

--- C17_2.py
+++ C17_2.py
@@ -9,14 +9,7 @@ class Hurdle:
     
     def paste_image(self, image):
         screen.blit(image, (self.rect.x, self.rect.y-14))
-'''
-class Thief:
-    def __init__(self, x, y, h, w):
-        self.rect = pygame.Rect(x, y, h, w)
-    
-    def draw_thief(self, surface):
-        pygame.draw.rect(surface, (31,40,45), self.rect)
-'''        
+        
 pygame.init()
 screen = pygame.display.set_mode((400, 400))
 
@@ -25,13 +18,7 @@ hurdle_2 = Hurdle(200, 198, 200, 2)
 
 hurdle_img = pygame.image.load("hurdle.png")
 hurdle_img = pygame.transform.scale(hurdle_img, (200,25))
-'''
-thief = Thief(10, 380, 15, 15)
 
-jewel = pygame.Rect(350, 10, 40, 40)
-jewel_image = pygame.image.load("jewel.png")
-jewel_image = pygame.transform.scale(jewel_image, (50,50))
-'''
 while True:
     screen.fill((255,249,238))
     for event in pygame.event.get():
@@ -41,10 +28,6 @@ while True:
     
     hurdle_1.paste_image(hurdle_img)
     hurdle_2.paste_image(hurdle_img)
-    '''
-    thief.draw_thief(screen)
-    
-    screen.blit(jewel_image, jewel)
-    '''
+   
     pygame.display.update()
     pygame.time.delay(10)
